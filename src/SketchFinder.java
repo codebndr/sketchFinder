bd8013a678ba04fd60a5f3acf563e0b533cf6811
kevin <wz246@cornell.edu>
2015-02-01 17:33:13 -0500

    add command line options to show/hide project and library files

--- src/SketchFinder.java
+++ src/SketchFinder.java
@@ -8,13 +8,39 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SketchFinder {
+	static FileTree tree = new FileTree(); // the tree will hold the entire
+	// file structures.
+	static AtomicBoolean useExtensions = new AtomicBoolean(true);
+	static AtomicBoolean showProjectFiles = new AtomicBoolean(false);
+	static AtomicBoolean showLibraryFiles = new AtomicBoolean(true);
+	static boolean inLibrary = false;
+
+	static private void parse(String[] args) {
+		tree.addRoot(new File(args[0]));
+		for (int i = 1; i < args.length; i++) {
+			if (args[i].equals("hideExtensions")) {
+				useExtensions.set(false);
+			} else if (args[i].equals("showProjectFiles")) {
+				showProjectFiles.set(true);
+			}
+			if (args[i].equals("hideLibraryFiles")) {
+				showLibraryFiles.set(false);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 
-		File folder = parse(args);
+		parse(args);
 
-		if (!folder.exists()) {
+		File folder = tree.getRoot();
+
+		System.out.println(folder.getName());
+
+		if (folder == null || !folder.exists()) {
 			System.err.print("The folder doesn't exist!");
 			return;
 		}
@@ -23,11 +49,8 @@ public class SketchFinder {
 			System.err.print("The folder is not a directory!");
 			return;
 		}
-
-		FileTree tree = new FileTree(folder); // the tree will hold the entire
-												// file structures.
 		FileTree libraries = null; // the libraries tree will hold the all the
-							// libraries in the folder.
+		// libraries in the folder.
 
 		// iterate the folder, add all the files with extension .ino or .pde to
 		// the files child.
@@ -37,25 +60,27 @@ public class SketchFinder {
 				// if the subfolder name is libraries folder, we will scan the
 				// folder and return a library tree.
 				if (f.getName().equals("libraries")) {
+					inLibrary = true;
 					libraries = getLibraries(f);
+					inLibrary = false;
+					continue;
 				}
-				
+
 				FileTree sub = getSketches(f);
 				if (sub != null) {
 					tree.addSubfolder(sub);
 				}
-			} else if (isIno(f) || isPde(f)) {
+			} else if (showProjectFiles.get()) {
 				tree.addFile(f);
 			}
 		}
 		System.out.println("Projects:");
 		System.out.println(tree);
-		
-		for(int i=0; i<70; i++) {
+
+		for (int i = 0; i < 70; i++) {
 			System.out.print("-");
 		}
 		System.out.println();
-//		System.out.println("Libraries:");
 		System.out.println(libraries);
 
 	}
@@ -74,9 +99,15 @@ public class SketchFinder {
 					tree.addSubfolder(sub);
 					hasStuff = true;
 				}
-			} else if (isIno(f) || isPde(f)) {
-				tree.addFile(f);
-				hasStuff = true;
+			} else {
+				if (inLibrary && showLibraryFiles.get()) {
+					tree.addFile(f);
+				} else if (!inLibrary && showProjectFiles.get()) {
+					tree.addFile(f);
+				}
+				if (isIno(f) || isPde(f)) {
+					hasStuff = true;
+				}
 			}
 		}
 
@@ -109,10 +140,22 @@ public class SketchFinder {
 		private List<FileTree> subfolders;
 		private List<File> files;
 
+		public FileTree() {
+			this(null);
+		}
+
+		public File getRoot() {
+			return folder;
+		}
+
 		public FileTree(File root) {
 			folder = root;
 			subfolders = new ArrayList<FileTree>();
 			files = new ArrayList<File>();
+		}
+
+		public void addRoot(File f) {
+			folder = f;
 		}
 
 		public void addSubfolder(FileTree sf) {
@@ -127,7 +170,8 @@ public class SketchFinder {
 		public String toString() {
 			return toString(0);
 		}
-		//sort the files and subfolders respectively and print them.
+
+		// sort the files and subfolders respectively and print them.
 		public String toString(int level) {
 			files.sort(new Comparator<File>() {
 				@Override
@@ -149,18 +193,21 @@ public class SketchFinder {
 				indentation += "    ";
 			}
 
-			StringBuffer output = new StringBuffer("> " + folder.getName() + "\n");
+			StringBuffer output = new StringBuffer("> " + folder.getName()
+					+ "\n");
 			for (File f : files) {
-				output.append(indentation).append(f.getName().substring(0, f.getName().lastIndexOf("."))).append("\n");
+				if (useExtensions.get()) {
+					output.append(indentation).append(f.getName()).append("\n");
+				} else {
+					output.append(indentation)
+							.append(f.getName().substring(0,
+									f.getName().lastIndexOf("."))).append("\n");
+				}
 			}
 			for (FileTree sub : subfolders) {
 				output.append(indentation).append(sub.toString(level));
 			}
 			return output.toString();
 		}
-	}
-
-	static private File parse(String[] args) {
-		return new File(args[0]);
 	}
 }
