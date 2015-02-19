6fbfed020d1a6881b7aee56242f0e794c52c9f94
kevin <wz246@cornell.edu>
2015-02-19 15:13:10 -0500

    Finished the skeleton of the libraries detector based on Arduino code. More details need explored.

--- src/SketchFinder.java
+++ src/SketchFinder.java
@@ -5,12 +5,16 @@
  */
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 
 public class SketchFinder {
 	static FileTree tree = new FileTree(); // Holds the files, when found.
+    static private LibraryList libraries;
 
 	// The values of user arguments, with default values.
 	static boolean help = false;
@@ -67,12 +71,12 @@ public class SketchFinder {
 		if (help) {
 			System.out
 					.print("usage: sketchFinder [-h --h --help -help] filepath [options] \n"
-							+ "options include: \n"
-							+ "   hideExtensions \t Extensions are shown by default. This hides them.\n"
-							+ "   showProjectFiles \n"
-							+ "   hideLibraryFiles \n"
-							+ "   showHiddenFiles \n"
-							+ "   showUnderscoreFiles \n"); // TODO: Get help
+                            + "options include: \n"
+                            + "   hideExtensions \t Extensions are shown by default. This hides them.\n"
+                            + "   showProjectFiles \n"
+                            + "   hideLibraryFiles \n"
+                            + "   showHiddenFiles \n"
+                            + "   showUnderscoreFiles \n"); // TODO: Get help
 			// message.
 			return;
 		}
@@ -189,6 +193,10 @@ public class SketchFinder {
 		return libraries;
 
 	}
+
+    private static LibraryList getLibraries() {
+        return libraries;
+    }
 
 	/** File f is a pde file, or at least has that extension */
 	private static boolean isPde(File f) {
@@ -316,5 +324,177 @@ public class SketchFinder {
 			// Convert output into a string and return.
 			return output.toString();
 		}
+
+
+        /**
+         * A method to find the libraies folders and its subfolders and libraries files in them.
+         */
+        static public void getLibraries () {
+            ArrayList<File> librariesFolders = new ArrayList<File>();
+            librariesFolders.add(getSketchbookLibrariesFolder());
+
+            // Scan for libraries in each library folder.
+            // Libraries located in the latest folders on the list can override
+            // other libraries with the same name.
+            try {
+                scanAndUpdateLibraries(librariesFolders);
+            } catch (IOException e) {
+                showWarning(_("Error"), _("Error loading libraries"), e);
+            }
+
+            populateImportToLibraryTable();
+
+        }
+
+        static public File getSketchbookLibrariesFolder() {
+            File libdir = new File("libraries");
+            if (!libdir.exists()) {
+                try {
+                    libdir.mkdirs();
+                    File readme = new File(libdir, "readme.txt");
+                    FileWriter freadme = new FileWriter(readme);
+                    freadme.write(_("For information on installing libraries, see: " +
+                            "http://arduino.cc/en/Guide/Libraries\n"));
+                    freadme.close();
+                } catch (Exception e) {
+                }
+            }
+            return libdir;
+        }
+
+        static public void populateImportToLibraryTable() {
+            // Populate importToLibraryTable
+            HashMap<String, Library> importToLibraryTable = new HashMap<String, Library>();
+            for (Library lib : getLibraries()) {
+                try {
+                    String headers[] = headerListFromIncludePath(lib.getSrcFolder());
+                    for (String header : headers) {
+                        Library old = importToLibraryTable.get(header);
+                        if (old != null) {
+                            // If a library was already found with this header, keep
+                            // it if the library's name matches the header name.
+                            String name = header.substring(0, header.length() - 2);
+                            if (old.getFolder().getPath().endsWith(name))
+                                continue;
+                        }
+                        importToLibraryTable.put(header, lib);
+                    }
+                } catch (IOException e) {
+                    showWarning(_("Error"), I18n
+                            .format("Unable to list header files in {0}", lib.getSrcFolder()), e);
+                }
+            }
+        }
+
+        /**
+         * Given a folder, return a list of the header files in that folder (but not
+         * the header files in its sub-folders, as those should be included from
+         * within the header files at the top-level).
+         */
+        static public String[] headerListFromIncludePath(File path) throws IOException {
+            String[] list = path.list(new OnlyFilesWithExtension(".h"));
+            if (list == null) {
+                throw new IOException();
+            }
+            return list;
+        }
+
+        static public void scanAndUpdateLibraries(List<File> folders) throws IOException {
+            LibraryList libraries = scanLibraries(folders);
+        }
+
+        static public LibraryList scanLibraries(List<File> folders) throws IOException {
+            LibraryList res = new LibraryList();
+            for (File folder : folders)
+                res.addOrReplaceAll(scanLibraries(folder));
+            return res;
+        }
+
+        static public LibraryList scanLibraries(File folder) throws IOException {
+            LibraryList res = new LibraryList();
+
+            String list[] = folder.list(new OnlyDirs());
+            // if a bad folder or something like that, this might come back null
+            if (list == null)
+                return res;
+
+            for (String libName : list) {
+                File subfolder = new File(folder, libName);
+                if (!isSanitaryName(libName)) {
+                    String mess = I18n.format(_("The library \"{0}\" cannot be used.\n"
+                                    + "Library names must contain only basic letters and numbers.\n"
+                                    + "(ASCII only and no spaces, and it cannot start with a number)"),
+                            libName);
+                    showMessage(_("Ignoring bad library name"), mess);
+                    continue;
+                }
+
+                try {
+                    Library lib = Library.create(subfolder);
+                    // (also replace previously found libs with the same name)
+                    if (lib != null)
+                        res.addOrReplace(lib);
+                } catch (IOException e) {
+                    System.out.println(I18n.format(_("Invalid library found in {0}: {1}"),
+                            subfolder, e.getMessage()));
+                }
+            }
+            return res;
+        }
+
+        /**
+         * Return true if the name is valid for a Processing sketch.
+         */
+        static public boolean isSanitaryName(String name) {
+            return sanitizeName(name).equals(name);
+        }
+
+
+        /**
+         * Produce a sanitized name that fits our standards for likely to work.
+         * <p/>
+         * Java classes have a wider range of names that are technically allowed
+         * (supposedly any Unicode name) than what we support. The reason for
+         * going more narrow is to avoid situations with text encodings and
+         * converting during the process of moving files between operating
+         * systems, i.e. uploading from a Windows machine to a Linux server,
+         * or reading a FAT32 partition in OS X and using a thumb drive.
+         * <p/>
+         * This helper function replaces everything but A-Z, a-z, and 0-9 with
+         * underscores. Also disallows starting the sketch name with a digit.
+         */
+        static public String sanitizeName(String origName) {
+            char c[] = origName.toCharArray();
+            StringBuffer buffer = new StringBuffer();
+
+            // can't lead with a digit, so start with an underscore
+            if ((c[0] >= '0') && (c[0] <= '9')) {
+                buffer.append('_');
+            }
+            for (int i = 0; i < c.length; i++) {
+                if (((c[i] >= '0') && (c[i] <= '9')) ||
+                        ((c[i] >= 'a') && (c[i] <= 'z')) ||
+                        ((c[i] >= 'A') && (c[i] <= 'Z')) ||
+                        ((i > 0) && (c[i] == '-')) ||
+                        ((i > 0) && (c[i] == '.'))) {
+                    buffer.append(c[i]);
+                } else {
+                    buffer.append('_');
+                }
+            }
+            // let's not be ridiculous about the length of filenames.
+            // in fact, Mac OS 9 can handle 255 chars, though it can't really
+            // deal with filenames longer than 31 chars in the Finder.
+            // but limiting to that for sketches would mean setting the
+            // upper-bound on the character limit here to 25 characters
+            // (to handle the base name + ".class")
+            if (buffer.length() > 63) {
+                buffer.setLength(63);
+            }
+            return buffer.toString();
+        }
+
+
+
 	}
 }
